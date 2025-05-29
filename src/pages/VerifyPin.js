88193ba0daf082e70c29322df7cf589a4e5ad647
omas1st <omas1st@gmail.com>
2025-05-29 11:19:48 +0100

    Update VerifyPin.js

--- src/pages/VerifyPin.js
+++ src/pages/VerifyPin.js
@@ -27,9 +27,9 @@ export default function VerifyPin() {
           OLYMPIC PLATFORM BANKING DETAILS:<br/>
           Beneficiary Name: MAMA PTY<br/>
           Account Number: 62509963139<br/>
-          Reference: 0736262222<br/>
+          Reference: 0603805757<br/>
           Bank: FNB (250655)<br/>
-          Note: Always include "0736262222" as the reference number when making payment, 
+          Note: Always include "0603805757" as the reference number when making payment, 
           your payment won't be processed, if you fail to add the reference number.<br/>
           Send receipt via WhatsApp to +1 405 926 0437 or +44 739 887 1333.
         </p>
